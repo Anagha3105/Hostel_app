6bcc60f0e6c5ede73c1868cefeff7dbd4e1ba0ff
Tanulr <73035034+Tanulr@users.noreply.github.com>
2021-02-27 16:58:42 +0530

    I'm such an idiot. Ok now it works.
    But it doesnt feel to impressive. Gimme more info I can make it more extensive.

--- tanu.py
+++ tanu.py
@@ -1,6 +1,6 @@
 
 #Referencing lists
-#using half the word so that it includes all possible ways of that word. like 'shar' will include 'share', 'sharing' etc.
+
 female = ['girl', 'female', 'wom', 'lady']
 male = ['boy', 'male', 'man', 'men']
 fee = ['fee', 'cost', 'price', 'pay']
@@ -11,31 +11,37 @@ block = ['1', 'first', '2', 'second', '3', 'third', '4', 'fourth', 'where', 'blo
 rules = ['rule'] #http://www.livingspaceblr.com/rules-and-regulations/
 
 #Getting input and converting it all to lowercase, to compare easily
-a = (input("Hey! Welcome to PESU RR Girls Hostel Portal. How may I help you today! \n")).lower()
-inp = a.split()
+t = (input("Hey! Welcome to PESU RR Girls Hostel Portal. How may I help you today! \n")).lower()
 
-#defining fucntions
+#Finding keywords to link to appropriate answer
+for i in fee:
+    if i in t:
+        print("Check out http://www.livingspaceblr.com/wp-content/uploads/2020/03/Fee-for-fresh-batch-2020-21-RR-Campus.pdf")
 
-if inp in fee:
-    print("Check out http://www.livingspaceblr.com/wp-content/uploads/2020/03/Fee-for-fresh-batch-2020-21-RR-Campus.pdf")
-if inp in application:
-    print("Check out http://web.archive.org/web/20191030074249/http://www.livingspaceblr.com/download-application-form/")
-if inp in amenities:
-    print("""•	A private and secured stay, with very high level security. 
-    •	Facilities for reading and entertainment.
-    •	Optional accommodation facility on single or shared bases.
-    •	Best of facilities with respect to maintenance and services.
-    •	Hygenic Food.
-    •	Transportation facility – from hostel to college and back to hostel.
-    •	Bio-metric Attendence system to make the presence of students tranparent.
-    •	CCTV camera survillence for the security of students at the premises of the hostel.
-    •	Beds with mattresses.
-    •	Built-in wardrobes.
-    •	Study chairs.
-    •	The hostel authorities shall arrange for clear, hygienic drinking water, 24 hours a day.
-    """)
-if inp in rules:
-    print("Check out http://www.livingspaceblr.com/rules-and-regulations/")
+for i in application:
+    if i in t:
+        print("Check out http://web.archive.org/web/20191030074249/http://www.livingspaceblr.com/download-application-form/")
+
+for i in amenities:
+    if i in t:
+        print("""•	A private and secured stay, with very high level security. 
+        •	Facilities for reading and entertainment.
+        •	Optional accommodation facility on single or shared bases.
+        •	Best of facilities with respect to maintenance and services.
+        •	Hygenic Food.
+        •	Transportation facility – from hostel to college and back to hostel.
+        •	Bio-metric Attendence system to make the presence of students tranparent.
+        •	CCTV camera survillence for the security of students at the premises of the hostel.
+        •	Beds with mattresses.
+        •	Built-in wardrobes.
+        •	Study chairs.
+        •	The hostel authorities shall arrange for clear, hygienic drinking water, 24 hours a day.
+        """)
+
+for i in rules:
+    if i in t:
+        print("Check out http://www.livingspaceblr.com/rules-and-regulations/")
+
 
 '''
 while True:
