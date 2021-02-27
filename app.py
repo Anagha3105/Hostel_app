1c669b9254613e33dd724a5c8abe52aa0788aee0
anushkajlr <79107280+anushkajlr@users.noreply.github.com>
2021-02-27 21:00:33 +0530

    21:00

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, redirect, render_template
+from flask_nav import Nav
+from flask_nav.elements import Navbar,Subgroup,View,Link,Text,Separator
 app = Flask(__name__)
+nav = Nav(app)
+nav.register_element('navbar',Navbar('mynav' , View('Home','index')))
 
 
 @app.route("/")
-def home():
-    return render_template("home.html")
+def index():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
