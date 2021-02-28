fe4e99b9303b8cdb582a314a5fa39b647cd55705
anushkajlr <79107280+anushkajlr@users.noreply.github.com>
2021-02-28 08:38:29 +0530

    8-38

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, redirect, render_template
-from flask_nav import Nav
-from flask_nav.elements import Navbar,Subgroup,View,Link,Text,Separator
+from flask import Flask, render_template, jsonify, request
+  
 app = Flask(__name__)
-nav = Nav(app)
-nav.register_element('navbar',Navbar('mynav' , View('Home','index')))
-
-
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+   return render_template("index.html")
+  
+@app.route('/my_form', methods=['POST'])
+def my_form():
+    form_input = request.form['comment_content']
+    # Now that get value back to server can send it to a DB(use Flask-SQLAlchemy)
+    return(form_input)
+ 
+if __name__ == '__main__':
+   app.run(debug=True)
